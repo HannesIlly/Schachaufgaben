d2f9975347d638f529d56b5b32aa90b2bc6c3a0d
Hannes Illy <Hannes.Illy@gmx.de>
2018-10-17 00:34:35 +0200

    Added methods in class Field to set and remove marks.

--- src/chessboard/Field.java
+++ src/chessboard/Field.java
@@ -4,9 +4,9 @@ package chessboard;
  * A field of a chess board consitsting of its colour and the piece that might be on it.
  */
 public class Field {
-    Colour colour;
-    Piece piece;
-    Mark mark; // todo - mark - HERE
+    private Colour colour;
+    private Piece piece;
+    private Mark mark;
 
     /**
      * Creates a new field with the given piece on it.
@@ -50,5 +50,21 @@ public class Field {
      */
     public void setPiece(Piece piece) {
         this.piece = piece;
+    }
+
+    /**
+     * Sets the mark of this field or removes it, if it is {@code null}.
+     *
+     * @param mark the mark of this field
+     */
+    public void setMark(Mark mark) {
+        this.mark = mark;
+    }
+
+    /**
+     * Removes any mark of this field.
+     */
+    public void unmark() {
+        setMark(null);
     }
 }
