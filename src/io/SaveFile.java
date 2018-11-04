70234288743de7ef555f941dba12d8fd02b1ceb1
Hannes Illy <Hannes.Illy@gmx.de>
2018-11-04 04:48:27 +0100

    Class SaveFile is now used to read/write Properties-files.

--- src/io/SaveFile.java
+++ src/io/SaveFile.java
@@ -11,93 +11,120 @@ import java.nio.Buffer;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Properties;
 
 import javax.imageio.ImageIO;
 
 /**
- * 
  * this class is used to read and write files
- * 
+ *
  * @author Hannes Illy
  * @version 1.0
- *
  */
 public class SaveFile {
+    private Properties properties = new Properties();
+
+    private String name;
     private String path;
     private File file;
 
     /**
-     * creates a SaveFile object that represents a file on the computer and will create the file or the directory too,
-     * if they don't exist
-     * 
-     * @param path
-     *            the path of the save file
+     * Creates a SaveFile object that represents a file on the computer and will create the file or
+     * the directory, if they don't exist
+     *
+     * @param name
+     *         The name of the save file. The file ending will be added.
+     * @throws IOException
+     *         if an io-error occurs
      */
-    public SaveFile(String path) {
-        this.path = path;
-        file = new File(this.path);
+    public SaveFile(String name) throws IOException {
+        this.name = name;
+        this.path = name + ".saps";
+        this.file = new File(this.path);
 
         if (!file.exists()) {
-            try {
-                file.mkdirs();
-                file.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
-
-    /**
-     * writes a list of strings into a file. Each string (list cell content) will be written into a separate line
-     * 
-     * @param list
-     *            the list, that will be written in the file
-     */
-    public void write(List<String> list) {
-        try {
-            FileWriter fileWriter = new FileWriter(file); // create file- and print writer
-            PrintWriter out = new PrintWriter(fileWriter);
-
-            Iterator<String> iterator = list.iterator(); // create new iterator (so the cursor is at the start of the list)
-            while (iterator.hasNext()) { // while there is a next list cell
-                out.println(iterator.next()); // write it in the file
-            }
-
-            out.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            file.mkdirs();
+            file.createNewFile();
         }
     }
 
     /**
-     * reads a file and returns its content in a list, in which each list cell contains a line of the file
-     * 
-     * @return the content of the file as a list of strings
+     * Writes the properties object to the file.
+     *
+     * @throws IOException if an io-error occurs
      */
-    public List<String> read() {
-        List<String> list = new LinkedList<String>();
+    public void writeProperties() throws IOException {
+        properties.store(new FileWriter(file), null);
+    }
 
-        try {
-            FileReader fileReader = new FileReader(file); // create file- and buffered reader
-            BufferedReader in = new BufferedReader(fileReader);
+    /**
+     * Reads the properties from the file.
+     *
+     * @throws IOException if an io-error occurs
+     */
+    public void readProperties() throws IOException {
+        properties.load(new FileReader(file));
+    }
 
-            list.add(in.readLine()); // add every new line at the end of the list
+    /**
+     * Gets the name of this file (without file ending).
+     *
+     * @return the name of the file
+     */
+    public String getName() {
+        return name;
+    }
 
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    /**
+     * Gets the property value of the key.
+     *
+     * @param key the key
+     * @return the value
+     * @throws IllegalArgumentException if the key is null or an empty string
+     */
+    public String get(String key) throws IllegalArgumentException {
+        if (key == null ||key.length() == 0) {
+            throw new IllegalArgumentException("Key muss vorhanden sein!");
         }
+        return properties.getProperty(key);
+    }
 
-        return list;
+    /**
+     * Checks if a property aleady exists.
+     *
+     * @param key the key
+     * @return if the key already exists
+     * @throws IllegalArgumentException if the key is null or an empty string
+     */
+    public boolean exists(String key) throws IllegalArgumentException {
+        if (key == null ||key.length() == 0) {
+            throw new IllegalArgumentException("Key muss vorhanden sein!");
+        }
+        return properties.getProperty(key) != null;
+    }
+
+    /**
+     * Sets the property value of the key to the value.
+     *
+     * @param key the key
+     * @return the value
+     * @throws IllegalArgumentException if the key is null or an empty string
+     */
+    public void set(String key, String value) throws IllegalArgumentException {
+        if (key == null ||key.length() == 0) {
+            throw new IllegalArgumentException("Key muss vorhanden sein!");
+        }
+        properties.setProperty(key, value);
     }
 
     /**
      * Reads an image from the given relative resource path.
      *
-     * @param path the relative path of the image
+     * @param path
+     *         the relative path of the image
      * @return the BufferedImage
-     * @throws IOException if the image cannot be read
+     * @throws IOException
+     *         if the image cannot be read
      */
     public static BufferedImage getImage(String path) throws IOException {
         return ImageIO.read(new File("resources/" + path));
@@ -106,10 +133,13 @@ public class SaveFile {
     /**
      * Writes an image to the specified path.
      *
-     * @param path the path of the image
-     * @param image the BufferedImage
+     * @param path
+     *         the path of the image
+     * @param image
+     *         the BufferedImage
      * @return if a writer could be created
-     * @throws IOException if the image cannot be written
+     * @throws IOException
+     *         if the image cannot be written
      */
     public static boolean writeImage(String path, BufferedImage image) throws IOException {
         return ImageIO.write(image, "png", new File(path));
